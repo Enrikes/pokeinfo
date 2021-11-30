eaf8f0403ac856f71240be82f4006c19a0cba44e
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2021-11-30 13:06:15 -0500

    Removed Test Folder

--- test/main.js
+++
@@ -1,9 +0,0 @@
-const pokeName = document.querySelector('.poke-name');
-const pokeWeight = document.querySelector('.poke-weight');
-const pokeHeight = document.querySelector('.poke-height');
-
-fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
-.then(res => res.json())
-.then(data => {
-    
-});
