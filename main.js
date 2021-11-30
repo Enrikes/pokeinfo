04c4ed18b6474b7e3208c18c8ba09862709464ba
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2021-11-30 13:07:29 -0500

    Feat: Added ability to read one pokemon from Poke API
    As of right now, we are able to read one pokemon and display its data on the webpage

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const pokeName = document.querySelector('.poke-name');
-const pokeWeight = document.querySelector('.poke-weight');
-const pokeHeight = document.querySelector('.poke-height');
 
-fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
+fetch('https://pokeapi.co/api/v2/pokemon/1')
 .then(res => res.json())
 .then(data => {
+    const pokeWeight = document.querySelector('.poke-weight');
+    const pokeHeight = document.querySelector('.poke-height');
+    const pokeName = document.querySelector('.poke-name');
+    const pokeTypeOne = document.querySelector ('.poke-type-one');
+    const pokeTypeTwo = document.querySelector('.poke-type-two');
+
+    pokeWeight.textContent = data['weight'];
+    pokeHeight.textContent = data['height'];
+    pokeName.textContent = data['name'];
+
+    const pokeType = data['types'];
+    const pokeFirstType = pokeType[0];
+    const pokeSecondType = pokeType[1];
     
+    pokeTypeOne.textContent = pokeFirstType['type']['name'];
+
+    if(pokeSecondType){
+        pokeTypeTwo.textContent = pokeSecondType['type']['name'];
+    } else {
+        pokeTypeTwo.classList.add('hide');
+        pokeSecondType.textContent = '';
+    }
+
+
 });
