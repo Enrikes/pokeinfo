64d0da086fa256d504fc5ba36186ad4f1fe261a8
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2021-12-02 17:52:59 -0500

    Cards can display Pokemon names.
    All the cards that are made can now display their proper names.

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ fetch('https://pokeapi.co/api/v2/pokemon/1')
 .then(data => {
     const pokeWeight = document.querySelector('.poke-weight');
     const pokeHeight = document.querySelector('.poke-height');
-    const pokeName = document.querySelector('.poke-name');
+    const pokeName = document.querySelectorAll('.poke-name');
     const pokeTypeOne = document.querySelector ('.poke-type-one');
     const pokeTypeTwo = document.querySelector('.poke-type-two');
-
+    const pokeImage = document.querySelectorAll('.poke-front-image');
+    console.log(data['sprites']['front_default']);
     pokeWeight.textContent = data['weight'];
     pokeHeight.textContent = data['height'];
     pokeName.textContent = data['name'];
-
+    document.getElementById("poke-front-image").src = data['sprites']['front_default'];
     const pokeType = data['types'];
     const pokeFirstType = pokeType[0];
     const pokeSecondType = pokeType[1];
@@ -23,7 +24,30 @@ fetch('https://pokeapi.co/api/v2/pokemon/1')
     } else {
         pokeTypeTwo.classList.add('hide');
         pokeSecondType.textContent = '';
-    }
-
-
+    };
 });
+
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=900')
+.then(res => res.json())
+.then(data => { 
+for (var i = 0; i < 1118; i+=1) {
+    // console.log(data.results[i].pokemon)
+
+};
+
+const pokeName = document.querySelectorAll('.poke-name');
+for (let i = 0; i < pokeName.length ; i++) {
+    const pokeName = document.querySelectorAll('.poke-name');
+    const pokeNameList = pokeName[i];
+    const resultData = data.results[i];
+    const { name } = resultData;
+    
+    if (resultData) {
+        pokeNameList.textContent = name;
+    } else {
+        pokeNameList.textContent = '';
+    } 
+} 
+
+})
