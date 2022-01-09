a41e0586ee62c749ca939d5b6abfd173d797e09c
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-01-08 22:29:11 -0500

    Rewrote Pokémon card to apply styles.

--- main.js
+++ main.js
@@ -13,15 +13,19 @@ const cardDesc = document.createElement("p");
 cardDesc.className = "cardDesc";
 //Grabs pokemon name
 let a = 0;
-function nextPoke(pokeOffset) {
-  console.log(a);
-  const pokeApi = `https://pokeapi.co/api/v2/pokemon?offset=${pokeOffset}limit=10`;
-  console.log(pokeOffset);
-  return pokeApi;
-}
+let offset = `offset=${a}&`;
+const pokeWindow = (offset) =>
+  `https://pokeapi.co/api/v2/pokemon?${offset}limit=10`;
+// console.log(pokeWindow());
+// function nextPoke() {
+//   a += 10;
+//   let pokeApi = pokeWindow(a);
+//   console.log(pokeApi);
+//   return pokeApi;
+// }
 async function grabPokemon() {
-  const pokeApi = "https://pokeapi.co/api/v2/pokemon?limit=10";
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+  const res = await fetch(pokeWindow());
+  console.log(res);
   const data = await res.json();
   const pokeArray = [];
   for (const pokemon of data.results) {
@@ -39,15 +43,34 @@ async function getSinglePokeUrl(url) {
 function createPokeCard(pokemon) {
   const card = document.createElement("div");
   card.className = "pokeCard";
-  card.className += " col-md-6";
+  card.id = "pokeCardId";
+  // card.className += " col-md";
   const pokemonName = pokemon.name;
   const pokeType = pokemon["types"];
   const pokeImage = pokemon["sprites"]["front_default"];
+  const pokeTypeOneDom = pokeType["0"]["type"]["name"];
+  const pokemonNameDom = document.createElement("h1");
+  pokemonNameDom.innerHTML = pokemonName;
 
-  card.innerHTML += `
-  <img id="pokeImage" src="${pokeImage}">
-  <h1>${pokemonName}</h1>
-  `;
+  const pokeImageDom = document.createElement("img");
+  const pokeImageContainer = document.createElement("div");
+  card.appendChild(pokeImageContainer);
+  pokeImageDom.src = pokeImage;
+  pokeImageDom.id = "poke-Image";
+  pokeImageContainer.appendChild(pokeImageDom);
+  card.appendChild(pokemonNameDom);
+
+  // card.innerHTML += `
+  // <div id='pokeImageContainer'><img id="pokeImage" src="${pokeImage}"> </div>
+  // <h1>${pokemonName}</h1>
+  // `;
+  if (pokeTypeOneDom === "grass") {
+    pokeImageContainer.classList.add("pokemon-grass");
+  } else if (pokeTypeOneDom === "fire") {
+    pokeImageContainer.classList.add("pokemon-fire");
+  } else if (pokeTypeOneDom === "water") {
+    pokeImageContainer.classList.add("pokemon-water");
+  }
   if (pokeType.length === 2) {
     const pokeTypeOneDom = pokeType["0"]["type"]["name"];
     const pokeTypeTwoDom = pokeType["1"]["type"]["name"];
@@ -58,13 +81,14 @@ function createPokeCard(pokemon) {
     pokeTypeDom.innerHTML = pokeTypeOneDom;
     card.appendChild(pokeTypeDom);
   }
+
   return card;
 }
 
 async function main() {
   // console.log(await createCard());
   const pokemonArray = await grabPokemon();
-  const pokeCardWrapper = document.querySelector(".row");
+  const pokeCardWrapper = document.querySelector(".pokeCardWrapper");
   for (const pokemon of pokemonArray) {
     pokeCardWrapper.appendChild(createPokeCard(pokemon));
   }
