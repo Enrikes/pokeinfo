c4f9cb6980c51b08eeea89a6a4f7c26bf09745b3
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2022-01-17 23:56:58 -0500

    Adding ability to cycle through different Pokémon's.

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import { createPokeCard } from "./PokeCard/pokeTypes.js";
 import { pokeSearch } from "./PokeSearch.js";
-const container = document.querySelector(".container");
-const cardDesc = document.createElement("p");
 
+const cardDesc = document.createElement("p");
 cardDesc.className = "cardDesc";
-export async function grabPokemon() {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+
+export async function grabPokemon(limit, offset) {
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  );
+  console.log(res);
   const data = await res.json();
   const pokeArray = [];
   for (const pokemon of data.results) {
@@ -14,7 +17,9 @@ export async function grabPokemon() {
 
   return pokeArray;
 }
-
+function poop() {
+  return grabPokemon(10, 20);
+}
 export async function getSinglePokeUrl(url) {
   const res = await fetch(url);
   const data = await res.json();
@@ -25,6 +30,7 @@ async function main() {
   const pokemonArray = await grabPokemon();
   const pokeCardWrapper = document.querySelector(".pokeCardWrapper");
   pokeSearch();
+  poop();
   for (const pokemon of pokemonArray) {
     pokeCardWrapper.appendChild(createPokeCard(pokemon));
   }
