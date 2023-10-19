fc04ea3e7631fa3fddfbb355294de6f0ed5f4226
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2023-10-19 09:22:57 -0400

    Unknown changes

--- nextPokemon.js
+++ nextPokemon.js
@@ -1,11 +1,21 @@
 import { grabPokemon } from "./main.js"
 import { getSinglePokeUrl } from "./main.js"
+import { createPokeCard } from "./PokeCard/pokeTypes.js"
     let a = 0;
 
-export function nextPokemon () {
+export async function nextPokemon () {
     a += 10;
     console.log(a)
     let b = 20;
-    const pokemon = grabPokemon(b, a)
+    const pokemon = grabPokemon(b, a);
+    const nextPokemonArray = [];
+    nextPokemonArray.push(pokemon)
+    console.log(nextPokemonArray);
+    for (const nextPokemonDom of nextPokemonArray) {
+        console.log(nextPokemonDom)
+    }
+      return nextPokemonArray
+
+    console.log(pokemon)
     console.log(getSinglePokeUrl(pokemon))
 }
