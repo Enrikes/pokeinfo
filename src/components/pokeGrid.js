80af5ebb744a6bed00a320f569b06dd1f7e6c6e4
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2023-10-27 14:36:02 -0400

    Feat: 20 Pokemon now display on load.

--- src/components/pokeGrid.js
+++ src/components/pokeGrid.js
@@ -3,21 +3,39 @@ import { useEffect, useState } from 'react';
 import PokeCard from './pokeCard/pokeCard';
 
 export default function PokeGrid({}) {
-  const [pokemon, setPokemon] = useState();
+  const [pokemon, setPokemon] = useState([]);
+  const [loadingStatus, setLoadingStatus] = useState('idle');
   const [isFetched, setIsFetched] = useState(false);
+  let pokemonID = 1;
 
   useEffect(() => {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/1`)
-      .then((res) => {
-        setPokemon(res.data);
-        setIsFetched(true);
-        console.log('Pokemon data:', res.data); // Add this for debugging
-      })
-      .catch((error) => {
-        console.error('Error fetching Pokemon:', error);
-      });
-  }, []);
+    const fetchAllPokemons = async () => {
+      try {
+        setLoadingStatus('loading');
 
-  return <div>{isFetched ? <PokeCard pokemon={pokemon} /> : null}</div>;
+        const requests = Array.from({ length: 20 }, (_, i) => {
+          return axios.get(`https://pokeapi.co/api/v2/pokemon/${i + 1}`);
+        });
+
+        const responses = await Promise.all(requests);
+        const allPokemons = responses.map((response) => response.data);
+        setIsFetched(true);
+        setLoadingStatus('loaded');
+        setPokemon(allPokemons);
+      } catch (error) {
+        console.error('Failed to fetch Pokémon data:', error);
+      }
+    };
+
+    fetchAllPokemons();
+  }, []);
+  console.log(pokemon);
+
+  return (
+    <div>
+      {loadingStatus == 'loaded' &&
+        pokemon.map((pokemons) => <PokeCard pokemon={pokemons} />)}
+      {loadingStatus == 'loading' && <p>Loading...</p>}
+    </div>
+  );
 }
