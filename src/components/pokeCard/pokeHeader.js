37d8417242e61a9a693eacb98a368a0e555a3892
Enrique <enricks12400@gmail.com>
2024-01-01 22:59:04 -0500

    Fix: Id's display with zeros.

--- src/components/pokeCard/pokeHeader.js
+++ src/components/pokeCard/pokeHeader.js
@@ -1,13 +1,17 @@
-import pokeImageCSS from './pokeHeader.module.css';
-import pokeHeaderCSS from './pokeType.module.css';
+import pokeImageCSS from "./pokeHeader.module.css";
+import pokeHeaderCSS from "./pokeType.module.css";
 export default function PokeHeader({ type, sprite, name, id }) {
+  const pokeHeader = pokeHeaderCSS[`pokemon-${type}`];
+  function formatID(id) {
+    return id.toString().padStart(4, "0");
+  }
   return (
     <header
       className={`${pokeImageCSS.pokeImageContainer} ${pokeHeaderCSS.rounded} ${pokeHeaderCSS[type]}`}
     >
       <h1>{name}</h1>
-      <img src={sprite} id={pokeImageCSS['poke-image']}></img>
-      <p className={pokeImageCSS.id}>#{id}</p>
+      <img src={sprite} id={pokeImageCSS["poke-image"]}></img>
+      <p className={pokeImageCSS.id}>#{formatID(id)}</p>
     </header>
   );
 }
