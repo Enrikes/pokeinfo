30be55861ae2682152dee36c4bfe7f9c9c1b6b1a
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2024-01-25 17:39:10 -0500

    Fix: Remove unused div tag

--- src/components/searchGrid.js
+++ src/components/searchGrid.js
@@ -16,23 +16,21 @@ export default function SearchGrid({
     setIsGridVisible(false);
   }
   return (
-    <div>
-      <div className={pokeGridCSS.grid}>
-        {searchedPokemon.map((pokemons, index) => (
-          <Link
-            to={`/${pokemons.name}`}
-            className={`${pokeGridCSS["link-no-underline"]} ${
-              pokeGridCSS["poke-anchor"]
-            } ${isGridVisible ? "" : "hidden"}`}
-            state={{ searchedPokemon: pokemons }}
-            key={pokemons.id}
-            onClick={handleClick}
-          >
-            <PokeCard pokemon={pokemons} className="pokeCard" />
-          </Link>
-        ))}
-        {loadingStatus === "loading" && <p>Loading...</p>}
-      </div>
+    <div className={pokeGridCSS.grid}>
+      {searchedPokemon.map((pokemons, index) => (
+        <Link
+          to={`/${pokemons.name}`}
+          className={`${pokeGridCSS["link-no-underline"]} ${
+            pokeGridCSS["poke-anchor"]
+          } ${isGridVisible ? "" : "hidden"}`}
+          state={{ searchedPokemon: pokemons }}
+          key={pokemons.id}
+          onClick={handleClick}
+        >
+          <PokeCard pokemon={pokemons} className="pokeCard" />
+        </Link>
+      ))}
+      {loadingStatus === "loading" && <p>Loading...</p>}
     </div>
   );
 }
